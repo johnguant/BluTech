66e2beb5d69bf7bbca99fd33fb7dfb5656798d78
BluB0lt <blu.b0lt12@gmail.com>
2013-10-14 21:13:45 +1300

    Updated Version System. Also a test

--- common/com/johnguant/blutech/lib/Reference.java
+++ common/com/johnguant/blutech/lib/Reference.java
@@ -23,6 +23,6 @@ public class Reference {
 	
 	public static final String MOD_ID = "blutech";
 	public static final String MOD_NAME = "BluTech";
-	public static final String VERSION = "0.0.0";
+	public static final String VERSION = "PreA1";
 	
 }
