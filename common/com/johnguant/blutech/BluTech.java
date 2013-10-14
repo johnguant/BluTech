8f202a42e3fabc4b4d01961ea8b64cdc34416441
Rhys Davies <rhys@johnguant.com>
2013-10-14 20:54:47 +1300

    Initial commit

---
+++ common/com/johnguant/blutech/BluTech.java
@@ -0,0 +1,51 @@
+/*
+ *   This file is part of BluTech.
+ *
+ *  BluTech is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  BluTech is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with BluTech.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ */
+
+package com.johnguant.blutech;
+
+import com.johnguant.blutech.lib.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class BluTech {
+	
+	@Instance(Reference.MOD_ID)
+	public static BluTech instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+
+}
