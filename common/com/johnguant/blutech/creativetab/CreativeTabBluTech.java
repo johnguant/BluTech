cd9dcc5525a9113cfa881fe1668719cb7a71a709
BluB0lt <blu.b0lt12@gmail.com>
2013-10-19 12:08:03 +1300

    CHanged the creative tab icon to BluDust

--- common/com/johnguant/blutech/creativetab/CreativeTabBluTech.java
+++ common/com/johnguant/blutech/creativetab/CreativeTabBluTech.java
@@ -1,5 +1,8 @@
 package com.johnguant.blutech.creativetab;
 
+import com.johnguant.blutech.item.BluItems;
+
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 
 public class CreativeTabBluTech extends CreativeTabs {
@@ -8,6 +11,11 @@ public class CreativeTabBluTech extends CreativeTabs {
 		super(tabID, tabLabel);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public int getTabIconItemIndex()
+    {
+        return BluItems.bluDust.itemID;
+    }
 	
 	
 
