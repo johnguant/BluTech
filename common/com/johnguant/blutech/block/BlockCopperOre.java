cf156cf2c45308e1b91647b6f5750d535bd81182
BluB0lt <blu.b0lt12@gmail.com>
2013-10-23 12:55:09 +1300

    Added Tin and Copper. Textured

---
+++ common/com/johnguant/blutech/block/BlockCopperOre.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * This file is part of BluTech.
+ *
+ * BluTech is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BluTech is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with BluTech.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+package com.johnguant.blutech.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+
+import com.johnguant.blutech.BluTech;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class BlockCopperOre extends Block {
+
+	public BlockCopperOre(int id) {
+		super(id, Material.rock);
+		setHardness(3.0F);
+		setStepSound(Block.soundStoneFootstep);
+		setUnlocalizedName("copperOre");
+		setCreativeTab(BluTech.tabsBluTech);
+		textureName = "copperOre";
+
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister iconRegister) {
+		this.blockIcon = iconRegister.registerIcon("blutech:"
+				+ (this.getUnlocalizedName().substring(5)));
+	}
+}
