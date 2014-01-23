8e21a43ca9eb9d78136b7e4219a9f975e40f7123
BluB0lt <blu.b0lt12@gmail.com>
2014-01-23 17:03:38 +1300

    Finally got around to nerfing Ore Gen. Rhys, can you check its ok

--- common/com/johnguant/blutech/gen/OreGen.java
+++ common/com/johnguant/blutech/gen/OreGen.java
@@ -46,21 +46,21 @@ public class OreGen implements IWorldGenerator {
 	private void generateOverworld(World world, Random random, int x, int z) {
 
 		this.addOreSpawn(BluBlocks.bluOre, world, random, x, z, 16, 16,
-				13 + random.nextInt(3), 5, 15, 70);
+				13 + random.nextInt(3), 5, 5, 30);
 		this.addOreSpawn(BluBlocks.tinOre, world, random, x, z, 16, 16,
 				12 + random.nextInt(3), 5, 15, 70);
 		this.addOreSpawn(BluBlocks.copperOre, world, random, x, z, 16, 16,
 				12 + random.nextInt(3), 5, 15, 70);
 		this.addOreSpawn(BluBlocks.leadOre, world, random, x, z, 16, 16,
-				7 + random.nextInt(3), 5, 15, 70);
+				7 + random.nextInt(3), 5, 15, 50);
 		this.addOreSpawn(BluBlocks.aluminiumOre, world, random, x, z, 16, 16,
-				10 + random.nextInt(3), 5, 15, 70);
+				10 + random.nextInt(3), 5, 15, 60);
 		this.addOreSpawn(BluBlocks.silverOre, world, random, x, z, 16, 16,
-				8 + random.nextInt(3), 5, 15, 70);
+				8 + random.nextInt(3), 5, 15, 50);
 		this.addOreSpawn(BluBlocks.rhodiumOre, world, random, x, z, 16, 16,
-				8 + random.nextInt(3), 5, 15, 70);
+				8 + random.nextInt(3), 5, 5, 16);
 		this.addOreSpawn(BluBlocks.remboniteOre, world, random, x, z, 16, 16,
-				5 + random.nextInt(3), 5, 15, 70);
+				5 + random.nextInt(3), 5, 5, 16);
 		
 	}
 
